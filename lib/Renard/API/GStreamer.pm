2ab1a57b6e26c6edd31d916c166f0d592ae20647
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-05-10 17:30:38 -0500

    Initial commit

---
+++ lib/Renard/API/GStreamer.pm
@@ -0,0 +1,11 @@
+package Renard::API::GStreamer;
+
+use strict;
+use warnings;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-API-GStreamer/>
+
+=cut
